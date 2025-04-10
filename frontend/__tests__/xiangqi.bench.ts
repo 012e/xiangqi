858261d46d48daf90b71923c829cb55681790472
012e <huyphmnhat@gmail.com>
2025-04-10 15:49:04 +0700

    add simple benchmark

--- frontend/__tests__/xiangqi.bench.ts
+++ frontend/__tests__/xiangqi.bench.ts
@@ -1,19 +1,21 @@
-import { bench, describe } from "vitest";
-import Xiangqi from "../src/lib/xiangqi";
-import { Chess } from "chess.js";
+import { bench, describe } from 'vitest';
+import Xiangqi from '../src/lib/xiangqi';
+import { Chess } from 'chess.js';
 
-describe("simple move", () => {
-  bench("our library", () => {
+describe('simple move', () => {
+  bench('our library', () => {
     const board = new Xiangqi();
-    board.move({from: "a4", to: "a5"});
-    board.move({from: "a7", to: "a6"});
-    board.move({from: "a5", to: "a6"});
+    board.move({ from: 'a4', to: 'a5' });
+    board.move({ from: 'a7', to: 'a6' });
+    board.move({ from: 'a5', to: 'a6' });
+    board.move({ from: 'a10', to: 'a9' });
   });
 
-  bench("chess.js", () => {
+  bench('chess.js', () => {
     const board = new Chess();
-    board.move({from: "a2", to: "a3"});
-    board.move({from: "a7", to: "a6"});
-    board.move({from: "a3", to: "a4"});
+    board.move({ from: 'a2', to: 'a3' });
+    board.move({ from: 'a7', to: 'a6' });
+    board.move({ from: 'a3', to: 'a4' });
+    board.move({ from: 'e7', to: 'e5' });
   });
 });
