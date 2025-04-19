a14c23c1e9028d6c0bbc0b493f1fa394d67f1bbb
012e <huyphmnhat@gmail.com>
2025-04-19 21:21:23 +0700

    config cors

--- backend/src/main/java/com/se330/ctuong_backend/config/CorsConfiguration.java
+++ backend/src/main/java/com/se330/ctuong_backend/config/CorsConfiguration.java
@@ -1,17 +1,8 @@
 package com.se330.ctuong_backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://xiangqii.netlify.app") // Allow requests from this origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow credentials (cookies, authorization headers, etc.)
-    }
 }
--- backend/src/main/java/com/se330/ctuong_backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/se330/ctuong_backend/config/WebSocketConfig.java
@@ -33,7 +33,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
