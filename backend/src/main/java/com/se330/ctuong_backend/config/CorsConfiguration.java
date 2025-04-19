c3e56963d2cf145c439b7c324557c977f4d6a44d
012e <huyphmnhat@gmail.com>
2025-04-19 17:58:51 +0700

    fix CORS settings

--- backend/src/main/java/com/se330/ctuong_backend/config/CorsConfiguration.java
+++ backend/src/main/java/com/se330/ctuong_backend/config/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
                 .allowCredentials(true);
     }
