0a6b14f1c7ffeb832dbc2d5db7d7348dba8e8730
012e <huyphmnhat@gmail.com>
2025-04-19 20:19:55 +0700

    add logging

--- backend/src/main/java/com/se330/ctuong_backend/config/CorsConfiguration.java
+++ backend/src/main/java/com/se330/ctuong_backend/config/CorsConfiguration.java
@@ -1,12 +1,18 @@
 package com.se330.ctuong_backend.config;
 
+import com.se330.ctuong_backend.util.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
+    @Autowired
+    private LoggingInterceptor loggingInterceptor;
+
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
     }
 }
