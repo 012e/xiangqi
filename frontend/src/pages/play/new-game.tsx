7f8f7a7f0343c9d73a65c678d9e4f8037d2e006b
loihnt05 <23520869@gm.uit.edu.vn>
2025-04-29 21:50:22 +0700

    divide newgame

--- frontend/src/pages/play/new-game.tsx
+++ frontend/src/pages/play/new-game.tsx
@@ -1,54 +1,18 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router';
-import { useStompClient, useSubscription } from 'react-stomp-hooks';
+// NewGame.tsx
 import { Loader2 } from 'lucide-react';
-import { toast } from 'sonner';
 import { Button } from '@/components/ui/button';
-
-type Game = {
-  gameId: string;
-  whitePlayerId: string;
-  blackPlayerId: string;
-};
+import { useCreateGame } from '../../stores/useCreateGame';
 
 export default function NewGame({ className }: { className?: string }) {
-  const { user } = useAuth0();
-  const stompClient = useStompClient();
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-  useSubscription(`/user/${user?.sub}/game/join`, (message) => {
-    const game = JSON.parse(message.body) as Game;
-    let player = 'white';
-    if (game.blackPlayerId === user?.sub) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      player = 'black';
-    }
-    navigate(`/game/${game.gameId}`);
-  });
+  const { createGame, loading, user } = useCreateGame();
 
   if (!user) {
     return <div>Not logged in yet</div>;
   }
 
-  function createGame() {
-    if (!stompClient) {
-      toast.error('Backend not connected');
-      return;
-    }
-    stompClient.publish({
-      headers: {
-        Authorization: 'Bearer ' + localStorage.getItem('access_token'),
-      },
-      destination: '/app/game/join',
-      body: JSON.stringify({}),
-    });
-    setLoading(true);
-  }
-
   if (loading) {
     return (
-      <Button disabled={true} className={className}>
+      <Button disabled className={className}>
         <div className="flex justify-center items-center h-16">
           <Loader2 className="w-6 h-6 animate-spin" />
         </div>
