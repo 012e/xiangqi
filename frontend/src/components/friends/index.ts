db4c0530807f6e66ab6f54f927624e9a6b32b913
loihnt05 <23520869@gm.uit.edu.vn>
2025-05-11 22:28:07 +0700

    fix index.ts in friends

--- frontend/src/components/friends/index.ts
+++ frontend/src/components/friends/index.ts
@@ -1,5 +1,5 @@
-import AddFriendForm from './addFriendForm';
-import FriendList from './friendList';
+import AddFriendForm from './AddFriendForm';
 import FriendChatHistory from './friendChatHistory';
+import FriendList from './FriendList';
 
 export { AddFriendForm, FriendList, FriendChatHistory };
