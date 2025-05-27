ac07d04837ac303dfb266eb719f493b82be7d2e3
loihnt05 <23520869@gm.uit.edu.vn>
2025-05-27 15:51:20 +0700

    uci fen save history

--- frontend/__tests__/fen.test.ts
+++ frontend/__tests__/fen.test.ts
@@ -1,5 +1,5 @@
 import Xiangqi from '../src/lib/xiangqi';
-import {describe, expect, it} from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 describe('generate valid fen', () => {
   it('should generate a valid initial FEN string', () => {
@@ -10,11 +10,35 @@ describe('generate valid fen', () => {
     );
   });
 
-    it("should be able to create game from custom fen", () => {
-        const xiangqi = new Xiangqi("rnbakabnr/9/1c5c1/p1p1P1p1p/4p4/9/P1P3P1P/1C5C1/9/RNBAKABNR w 3");
-        const s = xiangqi.exportFen();
-        expect(s).toBe(
-            'rnbakabnr/9/1c5c1/p1p1P1p1p/4p4/9/P1P3P1P/1C5C1/9/RNBAKABNR w 3',
-        );
-    })
+  it('should be able to create game from custom fen', () => {
+    const xiangqi = new Xiangqi(
+      'rnbakabnr/9/1c5c1/p1p1P1p1p/4p4/9/P1P3P1P/1C5C1/9/RNBAKABNR w 3',
+    );
+    const s = xiangqi.exportFen();
+    expect(s).toBe(
+      'rnbakabnr/9/1c5c1/p1p1P1p1p/4p4/9/P1P3P1P/1C5C1/9/RNBAKABNR w 3',
+    );
+  });
+});
+
+describe('generate valid uci fen', () => {
+  it('should generate a valid initial UCI FEN string', () => {
+    const xiangqi = new Xiangqi();
+    const s = xiangqi.exportUciFen();
+    expect(s).toBe(
+      'rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w 0',
+    );
+  });
+
+  it('should be able to create game from custom uci fen', () => {
+    const xiangqi = new Xiangqi(
+      'rnbakabnr/9/1c5c1/p1p1P1p1p/4p4/9/P1P3P1P/1C5C1/9/RNBAKABNR w 3 | c4c5 a7a6 e4e5',
+    );
+
+    const s = xiangqi.exportUciFen();
+
+    expect(s).toBe(
+      'rnbakabnr/9/1c5c1/p1p1P1p1p/4p4/9/P1P3P1P/1C5C1/9/RNBAKABNR w 3 | c4c5 a7a6 e4e5',
+    );
+  });
 });
