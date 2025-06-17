40d2d825cabb1c9b644d405b4f4acb10f4b21001
loihnt05 <23520869@gm.uit.edu.vn>
2025-06-17 12:44:34 +0700

    new branch

--- frontend/src/pages/test/test.tsx
+++ frontend/src/pages/test/test.tsx
@@ -1,11 +1,8 @@
-import { PieceStyleSelector } from "@/components/chessboard-styles/piece-setting";
 
 export default function Demo() {
 
   return (
     <div className="flex justify-center items-center h-screen">
-
-      <PieceStyleSelector/>
     </div>
   );
 }
