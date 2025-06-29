0de6001eb21e7adab7f011595b5490876de82e8b
012e <huyphmnhat@gmail.com>
2025-06-29 21:32:20 +0700

    --wip--

--- frontend/src/lib/friend/invite.ts
+++ frontend/src/lib/friend/invite.ts
@@ -1,11 +1,12 @@
 import { appAxios } from '@/services/AxiosClient';
 
 export async function inviteToGame(userId: number): Promise<void> {
-  const response = await appAxios.post(`/invitation/send/${userId}/5`, {
-    recipientId: userId,
-  });
-
-  if (response.status !== 201) {
-    throw new Error(`Failed to send invitation: ${response.statusText}`);
-  }
+  const response = await appAxios.post(
+    `/invitation/send/${userId}/5`,
+    {
+      recipientId: userId,
+    },
+    { validateStatus: (status) => status === 201 },
+  );
+  return response.data;
 }
